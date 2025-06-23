253f2c3450d1215736554be733a9ac652740584f
Harsh Vars <h.harshvats@gmail.com>
2025-06-23 13:43:36 +0530

    Changed name to ElevateCV

--- resume-reviewer-frontend/src/App.jsx
+++ resume-reviewer-frontend/src/App.jsx
@@ -66,7 +66,7 @@ function App() {
 
     return (
         <div className="App">
-            <Header siteName="ResumeFlow" />
+            <Header siteName="ElevateCV" />
             
             <main>
                 <UploadSection
